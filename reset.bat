64392a8129887d50b519b0c7dd16028cc7f767a6
Crown <Crown25352@example.com>
2023-12-12 13:41:35 -0500

    fix

---
+++ reset.bat
@@ -0,0 +1,6 @@
+git filter-branch -f --env-filter "
+    GIT_AUTHOR_NAME='Crown25352'
+    GIT_AUTHOR_EMAIL='Crown25352@outlook.com'
+    GIT_COMMITTER_NAME='Crown25352'
+    GIT_COMMITTER_EMAIL='Crown25352@outlook.com'
+  " HEAD
